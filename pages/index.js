5644678115dcf1198fe8d77870ab18b4c6c76648
joycelynmng <jmng399@gmail.com>
2022-09-12 20:34:34 -0700

    changed sum

--- pages/index.js
+++ pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <meta charset='utf-8' />
+        <meta charSet='utf-8' />
         <title>UCLA Food Places</title>
         <meta name='viewport' content='width=device-width, initial-scale=1' />
       </Head>
